e0eef3fffe381da7653a59a7680004f11f44fe2d
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-11-13 19:12:56 +0100

    next js api route

--- src/pages/blogs/[blogid]/index.tsx
+++ src/pages/blogs/[blogid]/index.tsx
@@ -1,8 +1,20 @@
 import { useParams } from "next/navigation";
+import { useEffect } from "react";
 
 const BlogPage = () => {
 	const params = useParams();
-    console.log(params);
+
+
+    const fetchData = async () => {
+        const res = await fetch('/api/hello');
+        const data = await res.json();
+        console.log(data);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
 	return <div>Blog ID : {params?.blogid}</div>;
 };
 export default BlogPage;
